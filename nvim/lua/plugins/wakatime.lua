a1719494684b45f13868d38532b08cb594fc7189
ScrimReaper <166304133+scrimreaper@users.noreply.github.com>
2026-01-13 19:45:26 +0100

    added angular plugin and markdown formatting

--- nvim/lua/plugins/wakatime.lua
+++ nvim/lua/plugins/wakatime.lua
@@ -1,3 +1,18 @@
 return {
   { "wakatime/vim-wakatime", lazy = false },
+
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+      -- Prettier for Markdown
+      opts.formatters_by_ft.markdown = { "prettier" }
+      opts.formatters_by_ft.markdown_inline = { "prettier" }
+      opts.formatters_by_ft["markdown.mdx"] = { "prettier" }
+      opts.formatters_by_ft.mdx = { "prettier" }
+
+      return opts
+    end,
+  },
 }
